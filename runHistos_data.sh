1b24f3dc2693fc71b1a123c18b6f56e76f304c0b
Dragos Velicanu <velicanu@mit.edu>
2015-12-09 11:28:36 +0100

    added parallel runHistos with new express runs

---
+++ runHistos_data.sh
@@ -0,0 +1,56 @@
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262548-v6.root\",\"/data/velicanu/zout/ZHIForestExpress_run262548-v6.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262620-v6.root\",\"/data/velicanu/zout/ZHIForestExpress_run262620-v6.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262656.root\",\"/data/velicanu/zout/ZHIForestExpress_run262656.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262694.root\",\"/data/velicanu/zout/ZHIForestExpress_run262694.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262695.root\",\"/data/velicanu/zout/ZHIForestExpress_run262695.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262697.root\",\"/data/velicanu/zout/ZHIForestExpress_run262697.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262703.root\",\"/data/velicanu/zout/ZHIForestExpress_run262703.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262726.root\",\"/data/velicanu/zout/ZHIForestExpress_run262726.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262733.root\",\"/data/velicanu/zout/ZHIForestExpress_run262733.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262734.root\",\"/data/velicanu/zout/ZHIForestExpress_run262734.root\",0\) &
+wait
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262735.root\",\"/data/velicanu/zout/ZHIForestExpress_run262735.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262768.root\",\"/data/velicanu/zout/ZHIForestExpress_run262768.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262777.root\",\"/data/velicanu/zout/ZHIForestExpress_run262777.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262783.root\",\"/data/velicanu/zout/ZHIForestExpress_run262783.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262784.root\",\"/data/velicanu/zout/ZHIForestExpress_run262784.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262811.root\",\"/data/velicanu/zout/ZHIForestExpress_run262811.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262813.root\",\"/data/velicanu/zout/ZHIForestExpress_run262813.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262814.root\",\"/data/velicanu/zout/ZHIForestExpress_run262814.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262815.root\",\"/data/velicanu/zout/ZHIForestExpress_run262815.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262816.root\",\"/data/velicanu/zout/ZHIForestExpress_run262816.root\",0\) &
+wait
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262817.root\",\"/data/velicanu/zout/ZHIForestExpress_run262817.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262818.root\",\"/data/velicanu/zout/ZHIForestExpress_run262818.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262819.root\",\"/data/velicanu/zout/ZHIForestExpress_run262819.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262834.root\",\"/data/velicanu/zout/ZHIForestExpress_run262834.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262836.root\",\"/data/velicanu/zout/ZHIForestExpress_run262836.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262837_preHBHE.root\",\"/data/velicanu/zout/ZHIForestExpress_run262837_preHBHE.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run262921.root\",\"/data/velicanu/zout/ZHIForestExpress_run262921.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263035.root\",\"/data/velicanu/zout/ZHIForestExpress_run263035.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263212.root\",\"/data/velicanu/zout/ZHIForestExpress_run263212.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263213.root\",\"/data/velicanu/zout/ZHIForestExpress_run263213.root\",0\) &
+wait
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263233.root\",\"/data/velicanu/zout/ZHIForestExpress_run263233.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263234.root\",\"/data/velicanu/zout/ZHIForestExpress_run263234.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263261.root\",\"/data/velicanu/zout/ZHIForestExpress_run263261.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263284.root\",\"/data/velicanu/zout/ZHIForestExpress_run263284.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263286.root\",\"/data/velicanu/zout/ZHIForestExpress_run263286.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263293.root\",\"/data/velicanu/zout/ZHIForestExpress_run263293.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263322.root\",\"/data/velicanu/zout/ZHIForestExpress_run263322.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263333.root\",\"/data/velicanu/zout/ZHIForestExpress_run263333.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263349.root\",\"/data/velicanu/zout/ZHIForestExpress_run263349.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263355.root\",\"/data/velicanu/zout/ZHIForestExpress_run263355.root\",0\) &
+wait
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263356.root\",\"/data/velicanu/zout/ZHIForestExpress_run263356.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263357.root\",\"/data/velicanu/zout/ZHIForestExpress_run263357.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263358.root\",\"/data/velicanu/zout/ZHIForestExpress_run263358.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263359.root\",\"/data/velicanu/zout/ZHIForestExpress_run263359.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263362.root\",\"/data/velicanu/zout/ZHIForestExpress_run263362.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263379.root\",\"/data/velicanu/zout/ZHIForestExpress_run263379.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263400.root\",\"/data/velicanu/zout/ZHIForestExpress_run263400.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263406.root\",\"/data/velicanu/zout/ZHIForestExpress_run263406.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263410.root\",\"/data/velicanu/zout/ZHIForestExpress_run263410.root\",0\) &
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263412.root\",\"/data/velicanu/zout/ZHIForestExpress_run263412.root\",0\) &
+wait
+root -l -b -q ggHistos.C+\(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/HIRun2015/HIExpressPhysics/Merged/HIForestExpress_run263491.root\",\"/data/velicanu/zout/ZHIForestExpress_run263491.root\",0\)
